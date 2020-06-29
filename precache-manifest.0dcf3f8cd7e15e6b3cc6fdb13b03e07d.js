432c2853af543e4b078da8504da4d5038dffdacb
HyungjunJeon <maru105kr@gmail.com>
2020-06-29 17:47:01 +0900

    Updates

---
+++ precache-manifest.0dcf3f8cd7e15e6b3cc6fdb13b03e07d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffddcd0b79afe34315a2a02fcd69e056",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "86e7b2a85fda894103f7",
+    "url": "/movie_app/static/css/main.0d166b81.chunk.css"
+  },
+  {
+    "revision": "c0071c60e1a78cbbff2e",
+    "url": "/movie_app/static/js/2.ce898f91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.ce898f91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86e7b2a85fda894103f7",
+    "url": "/movie_app/static/js/main.6637ea62.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
